effb4e0263d96dea30a77e86f780631483659d24
Teodes <jgrachot@gmail.com>
2022-07-25 22:19:10 -0300

    Landing Page In Progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import logo from "./logo.svg";
+import { Container, Navbar, Nav } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar variant="dark" expand="lg" fixed="top" className="mt-3">
+        <Container>
+          <Navbar.Brand href="#home">DANTO GAMES</Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            className="NavbarToggle"
+          />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#">Games</Nav.Link>
+              <Nav.Link href="#">News</Nav.Link>
+              <Nav.Link href="#">Gallery</Nav.Link>
+              <Nav.Link href="#">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className="LogoContainer position-relative">
+        <div className="imgBg"></div>
+        <img className="Logo" src={logo} width="800rem" alt="Logo" />
+        <div className="position-absolute">A</div>
+        <div>A</div>
+        <div>A</div>
+        <div>A</div>
+      </div>
     </div>
   );
 }
